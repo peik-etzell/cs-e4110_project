424519f256a1eb22bc51f71ff01f2794a05d5e40
Peik Etzell <peik.etzell@gmail.com>
2023-12-01 21:08:23 +0200

    typing problems

--- src/main/scala/reactor/Dispatcher.scala
+++ src/main/scala/reactor/Dispatcher.scala
@@ -5,17 +5,37 @@
 package reactor
 
 import reactor.api.{Event, EventHandler}
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.HashMap
+
+class WorkerThread[T](private val h: EventHandler[T]) extends Thread {
+  private var canceled = false;
+  override def run(): Unit = {
+    while (!canceled) {
+      val ev: T = h.getHandle.read()
+      h.handleEvent(ev)
+    }
+  }
+  def cancel() = canceled = true
+}
 
 final class Dispatcher(private val queueLength: Int = 10) {
   require(queueLength > 0)
 
+  val queue = new BlockingEventQueue(queueLength)
+  val handlers: HashMap[AnyRef, Thread] = new HashMap();
+// val mixedList: List[Any] = List(1, "hello", 3.14, true)
   @throws[InterruptedException]
-  def handleEvents(): Unit = ???
+  def handleEvents(): Unit = {
+    handlers.values foreach { h => h.run(); }
+  }
 
-  def addHandler[T](h: EventHandler[T]): Unit = ???
+  def addHandler[T](h: EventHandler[T]): Unit = {
+    handlers addOne (h, new WorkerThread[T](h));
+  }
 
-  def removeHandler[T](h: EventHandler[T]): Unit = ???
-
+  def removeHandler[T](h: EventHandler[T]): Unit = {
+  }
 }
 
 /*
