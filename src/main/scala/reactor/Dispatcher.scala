fa3bd12f11b31c2567ccfbb605fa77d94855bafd
Peik Etzell <peik.etzell@gmail.com>
2023-12-01 22:33:35 +0200

    Race conditioned success

--- src/main/scala/reactor/Dispatcher.scala
+++ src/main/scala/reactor/Dispatcher.scala
@@ -8,43 +8,58 @@ import reactor.api.{Event, EventHandler}
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.HashMap
 
-class WorkerThread[T](private val h: EventHandler[T]) extends Thread {
-  private var canceled = false;
+class WorkerThread[T](
+    private val q: BlockingEventQueue[Any],
+    private val h: EventHandler[T]
+) extends Thread {
+  var canceled = false;
   override def run(): Unit = {
     while (!canceled) {
-      val ev: T = h.getHandle.read()
-      h.handleEvent(ev)
+      // try {
+      val data = h.getHandle.read()
+      data match {
+        case null => this.canceled = true
+        case data => q.enqueue(new Event(data, h))
+      }
+      // } catch {
+      //   case e: InterruptedException => {
+      //     if (!canceled) throw e
+      //     println("canceled normally")
+      //   }
+      // }
     }
   }
-  def cancel() = canceled = true
+  def cancel() = {
+    canceled = true
+    interrupt()
+  }
 }
 
 final class Dispatcher(private val queueLength: Int = 10) {
   require(queueLength > 0)
 
-  val queue = new BlockingEventQueue(queueLength)
-  val handlers: HashMap[AnyRef, Thread] = new HashMap();
-// val mixedList: List[Any] = List(1, "hello", 3.14, true)
+  val q = new BlockingEventQueue[Any](queueLength)
+  val handlers: HashMap[EventHandler[_], WorkerThread[_]] = new HashMap();
+
   @throws[InterruptedException]
-  def handleEvents(): Unit = {
-    handlers.values foreach { h => h.run(); }
+  def handleEvents(): Unit = synchronized {
+    while (handlers.values.exists(h => !h.canceled) || q.getSize > 0) {
+      // println(
+      //   handlers.size + " handlers, running: " + handlers.values.count(h =>
+      //     !h.canceled
+      //   ) + ", cancelled: " + handlers.values.count(h => h.canceled)
+      // )
+      q.dequeue.dispatch()
+    }
   }
 
   def addHandler[T](h: EventHandler[T]): Unit = {
-    handlers addOne (h, new WorkerThread[T](h));
+    val worker = new WorkerThread[T](q, h)
+    worker.start()
+    handlers addOne { h -> worker };
   }
 
   def removeHandler[T](h: EventHandler[T]): Unit = {
+    handlers(h).cancel()
   }
 }
-
-/*
-// Hint:
-final class WorkerThread[T](???) extends Thread {
-
- override def run(): Unit = ???
-
- def cancelThread(): Unit = ???
-
-}
- */
