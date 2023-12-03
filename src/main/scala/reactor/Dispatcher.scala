1d95694ea06c0a26236e3ed9eff80cefe4984211
Peik Etzell <peik.etzell@gmail.com>
2023-12-03 10:10:34 +0200

    Use sentinel, should work

--- src/main/scala/reactor/Dispatcher.scala
+++ src/main/scala/reactor/Dispatcher.scala
@@ -8,58 +8,87 @@ import reactor.api.{Event, EventHandler}
 import scala.collection.mutable.ListBuffer
 import scala.collection.mutable.HashMap
 
-class WorkerThread[T](
-    private val q: BlockingEventQueue[Any],
-    private val h: EventHandler[T]
-) extends Thread {
-  var canceled = false;
-  override def run(): Unit = {
-    while (!canceled) {
-      // try {
-      val data = h.getHandle.read()
-      data match {
-        case null => this.canceled = true
-        case data => q.enqueue(new Event(data, h))
-      }
-      // } catch {
-      //   case e: InterruptedException => {
-      //     if (!canceled) throw e
-      //     println("canceled normally")
-      //   }
-      // }
-    }
-  }
-  def cancel() = {
-    canceled = true
-    interrupt()
-  }
-}
-
 final class Dispatcher(private val queueLength: Int = 10) {
   require(queueLength > 0)
 
-  val q = new BlockingEventQueue[Any](queueLength)
-  val handlers: HashMap[EventHandler[_], WorkerThread[_]] = new HashMap();
+  // Signal that a thread just stopped
+  private val workerStoppedSentinel = new Object
+
+  // Thread safe, mutated directly by worker threads
+  private val eventQueue = new BlockingEventQueue[Any](queueLength)
+
+  // handlerMap serves a dual role:
+  //    the keys is the set of added, non-removed handlers,
+  //    while the values contain all the running threads
+  //
+  // Rest of dispatcher single threaded, so this map can be very basic
+  private val handlerMap: HashMap[EventHandler[_], WorkerThread[_]] =
+    new HashMap();
+  private def workers = handlerMap.values
 
   @throws[InterruptedException]
-  def handleEvents(): Unit = synchronized {
-    while (handlers.values.exists(h => !h.canceled) || q.getSize > 0) {
-      // println(
-      //   handlers.size + " handlers, running: " + handlers.values.count(h =>
-      //     !h.canceled
-      //   ) + ", cancelled: " + handlers.values.count(h => h.canceled)
-      // )
-      q.dequeue.dispatch()
+  def handleEvents(): Unit = {
+    // A concurrency problem is the following:
+    // - A worker thread has to force the dispatcher to re-count the number of
+    //   running workers when the worker stops, otherwise the dispatcher can
+    //   be stuck waiting here forever
+    // - Solution:
+    //      - Strictly after switching to a non-running state,
+    //        send a `sentinel` message which does nothing
+    //        but wake up the dispatcher
+    while (
+      workers.exists(h => h.running) // Worker that could enqueue more events
+      || eventQueue.getSize > 0 // Existing events to handle
+    ) {
+      val event = eventQueue.dequeue
+      if (
+        // The sentinel should not and could not be dispatched
+        event.getData != workerStoppedSentinel
+        // A removed handler should not be dispatched to
+        && handlerMap.contains(event.getHandler)
+      ) {
+        event.dispatch()
+      }
     }
   }
 
   def addHandler[T](h: EventHandler[T]): Unit = {
-    val worker = new WorkerThread[T](q, h)
+    val worker = new WorkerThread[T](h)
     worker.start()
-    handlers addOne { h -> worker };
+    handlerMap.addOne { h -> worker };
   }
 
   def removeHandler[T](h: EventHandler[T]): Unit = {
-    handlers(h).cancel()
+    handlerMap(h).cancel()
+    handlerMap.remove(h) // Disallows dispatching to this handler
+  }
+
+  // Subclass of Dispatcher so queue etc. don't have to be passed
+  // along to constructor
+  private class WorkerThread[T](
+      private val h: EventHandler[T]
+  ) extends Thread {
+    private var _running = true;
+
+    def running = { _running }
+
+    override def run(): Unit = {
+      while (running) {
+        h.getHandle.read() match {
+          case null => _running = false
+          case data => eventQueue enqueue new Event(data, h)
+        }
+      }
+      // Signal that this worker is now done; no more messages will be sent
+      // Its `running` status will always be false here, so that 
+      // when the dispatcher checks again, this worker is not running
+      eventQueue enqueue new Event(workerStoppedSentinel.asInstanceOf[T], h)
+    }
+
+    // Does not need to be interrupted, reading an extra time is allowed,
+    // while the dispatching after cancel was disallowed
+    def cancel() = {
+      _running = false
+    }
   }
 }
